77e6f850609d361cabb9f7141832430290e70435
ganiefah <ganiefahmajiet3@gmail.com>
2023-05-16 14:51:31 +0200

    done

---
+++ script/code.js
@@ -0,0 +1,17 @@
+// question2
+let subjects  = ['Python', 'C++','lippe', 'VB.Net',' Javascript' ,'Assembly'];
+
+
+let values = subjects.length
+console.log(values);
+
+console.log(subjects.pop());
+
+// subjects.pop();
+// console.log(subjects.pop(2));
+
+if ((subjects.length%2)==0) {
+    console.log(subjects[output-1]+' '+subjects[output])
+}else{
+    console.log(subjects[output])
+}
